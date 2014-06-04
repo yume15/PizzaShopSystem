91e0e4fb2cd795a2a7f31929b744acca79f99d5f
yume15 <g12915iy@gm.tsuda.ac.jp>
2014-06-04 16:43:18 +0900

    データを追加する機能

---
+++ mygae/src/jp/iwasaki/PMF.java
@@ -0,0 +1,14 @@
+package jp.iwasaki;
+
+import javax.jdo.*;
+
+public class PMF {
+    private static final PersistenceManagerFactory factory = 
+        JDOHelper.getPersistenceManagerFactory("transactions-optional");
+     
+    private PMF(){}
+     
+    public static PersistenceManagerFactory get(){
+        return factory;
+    }
+}
